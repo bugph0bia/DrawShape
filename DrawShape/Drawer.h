5972efe82b94352353c4761572a06e781115a6ac
mat2umoto <mat2umoto.kentar0@gmail.com>
2019-12-01 01:18:30 +0900

    Nodeの派生クラス以外を追加

--- DrawShape/Drawer.h
+++ DrawShape/Drawer.h
@@ -8,7 +8,15 @@
 #include <stdexcept>
 #include <cmath>
 
+// std::string/wstringのサンク対応
+#ifdef UNICODE
+#define tstring wstring
+#else
+#define tstring string
+#endif
+
 // std::numeric_limits<T>::max, min を使用するため
+// このヘッダファイルをインクルードした後はマクロ関数のmax,minは使用できないので注意
 #undef max
 #undef min
 
@@ -194,12 +202,12 @@ public:
 	// 矢印先端描画
 	void DrawArrowHead(const Coord<double>& start, const Coord<double>& end);
 	// ベジエ曲線による円弧描画
-	void DrawBezierArc();
+	void DrawBezierArc(Coord<double> start, Coord<double> end, Coord<double> center, bool rht);
 
-	// 描画内容をファイル保存
-	bool SaveBitmap(const std::string& filePath) const;
+	// 描画内容をファイル保存(BMP/PNG/JPEG/GIF)
+	bool SaveImage(const std::tstring& filePath) const;
 	// 描画内容をクリップボードへコピー
-	bool CopyBitmap() const;
+	bool CopyImage(CWnd* pOwner) const;
 
 	// デバイスコンテキストを取得
 	CDC* GetDC() const { return m_pDC; };
